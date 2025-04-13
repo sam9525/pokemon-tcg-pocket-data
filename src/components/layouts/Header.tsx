c7652b8530b5df049c90b75f41029bc45c52e020
sam9525 <sam9985246327@gmail.com>
2025-04-14 00:44:30 +0930

    Change font size in header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
       <header className="w-full bg-foreground">
         <div className="max-w-4xl mx-auto px-4 pt-8">
           <div className="flex justify-center mb-8">
-            <Link className="text-primary text-4xl font-bold" href={"/"}>
+            <Link className="text-primary text-3xl font-bold" href={"/"}>
               POKEMON TCG POCKET
             </Link>
           </div>
