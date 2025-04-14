7517ec739f2e343439d2742761e5dbaaa2fa75eb
sam9525 <sam9985246327@gmail.com>
2025-04-14 14:08:47 +0930

    Add user button menu

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import HeaderTabs from "./HeaderTabs";
 import User from "../icons/user";
+import Earth from "../icons/earth";
+import Login from "../icons/login";
 
 export default function Header() {
   return (
@@ -16,6 +18,23 @@ export default function Header() {
             <button className="w-9 absolute right-13 cursor-pointer">
               <User />
             </button>
+            <div className="w-42 absolute top-20 right-11 border-1 border-primary bg-background rounded-md px-4 py-2 flex flex-col items-start justify-start">
+              <div className="flex items-center justify-start mb-2 cursor-pointer">
+                <div className="w-6">
+                  <Earth />
+                </div>
+                <span className="ml-2 text-sm">語言：繁體中文</span>
+              </div>
+              <Link
+                className="flex items-center justify-start cursor-pointer"
+                href="/login"
+              >
+                <div className="w-6">
+                  <Login />
+                </div>
+                <span className="ml-2 text-sm">登入</span>
+              </Link>
+            </div>
           </div>
           <hr className="text-primary w-full border-primary border-2"></hr>
           <HeaderTabs />
