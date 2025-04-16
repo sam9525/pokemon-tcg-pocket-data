5235e8121a06525fe77cfddde96bda043b1f03a8
sam9525 <sam9985246327@gmail.com>
2025-04-16 14:27:14 +0930

    Add zod library for user input data schema validation

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { ZodError } from "zod";
 import { saltAndHashPassword } from "@/utils/password";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -11,21 +12,28 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
-        let user = null;
+        try {
+          let user = null;
 
-        // logic to salt and hash password
-        const pwHash = saltAndHashPassword(credentials.password);
+          // logic to salt and hash password
+          const pwHash = saltAndHashPassword(credentials.password);
 
-        // logic to verify if the user exists
-        user = await getUserFromDb(credentials.email, pwHash);
+          // logic to verify if the user exists
+          user = await getUserFromDb(credentials.email, pwHash);
 
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          throw new Error("Invalid credentials.");
+          if (!user) {
+            // No user found, so this is their first attempt to login
+            throw new Error("Invalid credentials.");
+          }
+
+          // return user object with their profile data
+          return user;
+        } catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null;
+          }
         }
-
-        // return user object with their profile data
-        return user;
       },
     }),
   ],
