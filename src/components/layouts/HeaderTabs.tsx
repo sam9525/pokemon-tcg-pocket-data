750a5d4b850f9eff013e7e58d5aafb5d4829818f
samraber <sam9985246327@gmail.com>
2025-04-19 21:28:17 +0930

    Display more header tabs after logged in

--- src/components/layouts/HeaderTabs.tsx
+++ src/components/layouts/HeaderTabs.tsx
@@ -2,7 +2,12 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+
 export default function HeaderTabs() {
+  const session = useSession();
+  const status = session?.status;
+
   const path = usePathname();
   return (
     <div className="flex justify-center font-bold header-tabs">
@@ -18,6 +23,28 @@ export default function HeaderTabs() {
       <Link className={path === "/search" ? "active" : ""} href={"/search"}>
         搜尋
       </Link>
+      {status === "authenticated" && (
+        <>
+          <Link
+            className={path === "/my-decks" ? "active" : ""}
+            href={"/my-decks"}
+          >
+            我的牌組
+          </Link>
+          <Link
+            className={path === "/deck-collection" ? "active" : ""}
+            href={"/deck-collection"}
+          >
+            牌組收藏
+          </Link>
+          <Link
+            className={path === "/deck-builder" ? "active" : ""}
+            href={"/deck-builder"}
+          >
+            組牌
+          </Link>
+        </>
+      )}
     </div>
   );
 }
