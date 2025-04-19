7327f83746fb12108251c2db2c7df18ecdf0f1f2
samraber <sam9985246327@gmail.com>
2025-04-19 14:52:04 +0930

    Change log in to log out after logged in

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -7,8 +7,13 @@ import Earth from "../icons/earth";
 import Login from "../icons/login";
 import { useState, useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
+import Logout from "../icons/logout";
 
 export default function Header() {
+  const session = useSession();
+  const userData = session.data?.user;
+  const userName = userData?.name;
   const [showUserMenu, setShowUserMenu] = useState(false);
   const pathname = usePathname();
   const menuRef = useRef<HTMLDivElement>(null);
@@ -53,21 +58,38 @@ export default function Header() {
                 ref={menuRef}
                 className="w-42 absolute top-20 right-11 border-1 border-primary bg-background rounded-md px-4 py-2 flex flex-col items-start justify-start"
               >
+                {session.status === "authenticated" && (
+                  <div className="text-sm">Hello, {userName}</div>
+                )}
                 <div className="flex items-center justify-start mb-2 cursor-pointer">
                   <div className="w-6">
                     <Earth />
                   </div>
                   <span className="ml-2 text-sm">語言：繁體中文</span>
                 </div>
-                <Link
-                  className="flex items-center justify-start cursor-pointer"
-                  href="/login"
-                >
-                  <div className="w-6">
-                    <Login />
-                  </div>
-                  <span className="ml-2 text-sm">登入</span>
-                </Link>
+                {session.status !== "authenticated" && (
+                  <Link
+                    className="flex items-center justify-start cursor-pointer"
+                    href="/login"
+                  >
+                    <div className="w-6">
+                      <Login />
+                    </div>
+                    <span className="ml-2 text-sm">登入</span>
+                  </Link>
+                )}
+                {session.status === "authenticated" && (
+                  <Link
+                    className="flex items-center justify-start cursor-pointer"
+                    href="/"
+                    onClick={() => signOut()}
+                  >
+                    <div className="w-6">
+                      <Logout />
+                    </div>
+                    <span className="ml-2 text-sm">登出</span>
+                  </Link>
+                )}
               </div>
             )}
           </div>
