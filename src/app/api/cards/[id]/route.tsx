89366de421d6557e52ad5dc201fd1d5f7a983112
Sam Huang <sam9985246327@gmail.com>
2025-09-23 00:35:15 +0930

    Fix incorrect api route

--- src/app/api/cards/[id]/route.tsx
+++ src/app/api/cards/[id]/route.tsx
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     // Get filter from URL search params
