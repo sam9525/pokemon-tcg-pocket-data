55a20ac614e16976a549cde55fcd5d54215737bf
Sam Huang <sam9985246327@gmail.com>
2025-11-14 14:24:51 +1030

    Docker prod and dev yml

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,16 @@ const nextConfig: NextConfig = {
     ],
   },
   output: "standalone",
+  webpack: (config, {}) => {
+    // Enable polling for file watching in Docker
+    if (process.env.CHOKIDAR_USEPOLLING === "true") {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300, // Delay before rebuilding
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
