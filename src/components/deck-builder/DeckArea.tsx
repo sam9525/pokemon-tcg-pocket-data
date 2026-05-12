be7d904475b24a17cda49fbbebcf5665ca52d5e7
Sam Huang <sam9985246327@gmail.com>
2026-05-12 23:03:02 +0930

    feat: add horizontal scroll to deck area when more than 10 cards

--- src/components/deck-builder/DeckArea.tsx
+++ src/components/deck-builder/DeckArea.tsx
@@ -105,26 +105,26 @@ export default function DeckArea({
         </span>
       </div>
 
-      {/* Card list - always show 10 slots */}
-      <div className="flex flex-row gap-3 overflow-x-auto pb-2 py-2 -mr-3">
-        {Array.from({ length: 10 }).map((_, i) => {
-          const card = cards[i];
-          return card ? (
-            <DeckCard
-              key={card.cardId}
-              cardId={card.cardId}
-              imageUrl={cardImages[card.cardId] || ""}
-              quantity={card.quantity}
-              onRemoveOne={onRemoveOne}
-              onRemoveAll={onRemoveAll}
-            />
-          ) : (
+      {/* Card list - scrollable when more than 10 cards */}
+      <div className="flex flex-row gap-3 overflow-x-auto pb-2 py-2 -mr-3 scrollbar">
+        {cards.map((card) => (
+          <DeckCard
+            key={card.cardId}
+            cardId={card.cardId}
+            imageUrl={cardImages[card.cardId] || ""}
+            quantity={card.quantity}
+            onRemoveOne={onRemoveOne}
+            onRemoveAll={onRemoveAll}
+          />
+        ))}
+        {/* Empty slots only when less than 10 cards */}
+        {cards.length < 10 &&
+          Array.from({ length: 10 - cards.length }).map((_, i) => (
             <div
               key={`placeholder-${i}`}
               className="w-20 h-28 sm:w-24 sm:h-32 bg-gray-600/40 border-2 border-dashed border-gray-300 rounded-lg flex-shrink-0"
             />
-          );
-        })}
+          ))}
       </div>
     </div>
   );
